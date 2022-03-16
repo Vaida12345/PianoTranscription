0a5db854ac1dd746850c673d2e06e3b61de88ec0
Vaida <91354917+Vaida12345@users.noreply.github.com>
2022-03-16 14:46:52 +0800

    Initial Commit

---
+++ PianoTranscription/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PianoTranscription
+//
+//  Created by Vaida on 3/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PianoTranscription/PianoTranscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoTranscriptionApp.swift
+//  PianoTranscription
+//
+//  Created by Vaida on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct PianoTranscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
